40ed9171704681e610467cf25d77f0ed6e178e89
Austen Stone <miltonlaxer@gmail.com>
2021-02-25 12:02:51 -0500

    update angular core

--- src/app/watch/watch.component.spec.ts
+++ src/app/watch/watch.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WatchComponent } from './watch.component';
 
@@ -6,7 +6,7 @@ describe('WatchComponent', () => {
   let component: WatchComponent;
   let fixture: ComponentFixture<WatchComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WatchComponent ]
     })
